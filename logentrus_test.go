2d34bce197430471eeded6f268a1c23f1abd55a1
Jonathan Robertson <5396045+jonathan-robertson@users.noreply.github.com>
2016-12-10 14:11:05 -0600

    Update package name logrusentries to logentrus

--- logentrus_test.go
+++ logentrus_test.go
@@ -1,17 +1,18 @@
-package logrusentries
+package logentrus_test
 
 import (
 	"os"
 	"testing"
 
 	"github.com/Sirupsen/logrus"
+	"github.com/puddingfactory/logentrus"
 )
 
 func init() {
 	logrus.SetLevel(logrus.DebugLevel)           // This will effect your stdout level, but not the level for LogentriesHook. You specify that priority on creation
 	logrus.SetFormatter(&logrus.TextFormatter{}) // You an use any formatter; LogentriesHook will always format as JSON without interfering with your other hooks
 
-	hook, err := NewLogentriesHook(
+	hook, err := logentrus.New(
 		os.Getenv("TOKEN"), // fetching token from env vars here. You can make a token in your logentries account and are expected to have 1 token for each application
 		"Jan 2 15:04:05",   // timeFormat could be an empty string instead; doing so will default to logrus's typically time format.
 		logrus.InfoLevel,   // log level is inclusive. Setting to logrus.ErrorLevel, for example, would include errors, panics, and fatals, but not info or debug.
