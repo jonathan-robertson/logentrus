c7716d39c23a8d202e1a8109843a91395cf1bc90
Jonathan Robertson <5396045+jonathan-robertson@users.noreply.github.com>
2017-01-29 18:45:36 -0600

    Update to support unencrypted TCP & UDP modes close #1

--- logentrus.go
+++ logentrus.go
@@ -13,37 +13,86 @@ import (
 
 // Hook used to send logs to logentries
 type Hook struct {
-	levels    []logrus.Level
+	encrypt   bool
 	token     string
+	levels    []logrus.Level
 	formatter *logrus.JSONFormatter
+	network   string
+	port      int
 	tlsConfig *tls.Config
 }
 
+// Opts is a set of optional parameters for NewEncrytpedHook
+type Opts struct {
+	Priority        logrus.Level // defaults to logrus.DebugLevel (include all)
+	TimestampFormat string       // defaults to logrus's typical timestamp format
+	EncTLSConfig    *tls.Config  // defaults to use system's cert store; provide if you'd like to enforce your own root certs
+	UnencryptedTCP  bool         // defaults to false (encryption enabled, using TCP)
+	UnencryptedUDP  bool         // defaults to false (encryption enabled, using TCP)
+	UnencryptedPort int          // defaults to 80; available ports are 80, 514, and 10000
+}
+
 const (
-	version = "v1.0.2"
+	version = "v2.0.0"
 	host    = "data.logentries.com"
-	port    = 443
+	tlsPort = 443
 )
 
-// New creates and returns a new hook for use in Logrus
-func New(token, timestampFormat string, priority logrus.Level, config *tls.Config) (hook *Hook, err error) {
+var (
+	errTokenRequired = fmt.Errorf("unable to create new hook: a Token is required")
+	errInvalidPort   = fmt.Errorf("unable to create new unencrypted hook: invalid port provided; only 80, 514, and 10000 are supported")
+)
+
+// New creates and returns a Logrus hook for Logentries Token-based logging
+// ref: https://docs.logentries.com/docs/input-token
+func New(token string, options *Opts) (hook *Hook, err error) {
 	if token == "" {
-		err = fmt.Errorf("Unable to create new LogentriesHook since a Token is required")
+		err = errTokenRequired
 	} else {
 		hook = &Hook{
-			levels:    logrus.AllLevels[:priority+1], // Include all levels at or within the provided priority
+			encrypt:   true,
 			token:     token,
-			tlsConfig: config,
-			formatter: &logrus.JSONFormatter{
-				TimestampFormat: timestampFormat,
-			},
+			levels:    logrus.AllLevels,
+			formatter: &logrus.JSONFormatter{},
+			network:   "tcp",
+			port:      tlsPort,
+		}
+
+		if options != nil {
+			hook.formatter.TimestampFormat = options.TimestampFormat
+			hook.levels = logrus.AllLevels[:options.Priority+1]
+
+			switch {
+			case options.UnencryptedTCP:
+				hook.encrypt = false
+				hook.network = "tcp"
+				hook.port = 514
+			case options.UnencryptedUDP:
+				hook.encrypt = false
+				hook.network = "udp"
+				hook.port = 514
+			}
+
+			if hook.encrypt {
+				if options.EncTLSConfig != nil {
+					hook.tlsConfig = options.EncTLSConfig
+				}
+			} else {
+				switch options.UnencryptedPort {
+				case 80, 514, 10000:
+					hook.port = options.UnencryptedPort
+				case 0: // ignore
+				default:
+					err = errInvalidPort
+					return
+				}
+			}
 		}
 
 		// Test connection
 		if conn, err := hook.dial(); err == nil {
 			conn.Close()
 		}
-
 	}
 	return
 }
@@ -57,20 +106,23 @@ func (hook *Hook) Levels() []logrus.Level {
 func (hook *Hook) Fire(entry *logrus.Entry) (err error) {
 	line, err := hook.format(entry)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "Unable to read entry | err: %v | entry: %+v\n", err, entry)
+		fmt.Fprintf(os.Stderr, "unable to read entry | err: %v | entry: %+v\n", err, entry)
 		return err
 	}
 
 	if err = hook.write(line); err != nil {
-		fmt.Fprintf(os.Stderr, "ERROR: Unable to write to conn | err: %v | line: %s\n", err, line)
+		fmt.Fprintf(os.Stderr, "ERROR: unable to write to conn | err: %v | line: %s\n", err, line)
 	}
 
 	return
 }
 
-// dial establishes a new connection which caller responsible for closing
+// dial establishes a new connection which caller is responsible for closing
 func (hook Hook) dial() (net.Conn, error) {
-	return tls.Dial("tcp", fmt.Sprintf("%s:%d", host, port), hook.tlsConfig)
+	if hook.encrypt {
+		return tls.Dial(hook.network, fmt.Sprintf("%s:%d", host, hook.port), hook.tlsConfig)
+	}
+	return net.Dial(hook.network, fmt.Sprintf("%s:%d", host, hook.port))
 }
 
 // write dials a connection and writes token and line in bytes to connection
--- logentrus_test.go
+++ logentrus_test.go
@@ -14,9 +14,14 @@ func init() {
 
 	hook, err := logentrus.New(
 		os.Getenv("LOGENTRIESTOKEN"), // fetching token from env vars here. You can make a token in your logentries account and are expected to have 1 token for each application
-		"Jan 2 15:04:05",             // timeFormat could be an empty string instead; doing so will default to logrus's typically time format.
-		logrus.InfoLevel,             // log level is inclusive. Setting to logrus.ErrorLevel, for example, would include errors, panics, and fatals, but not info or debug.
-		nil,                          // setting config to nil means that conn will use root certs already set up on local system
+		&logentrus.Opts{ // include options (set to nil if options not necessary)
+			Priority:        logrus.InfoLevel, // log level is inclusive. Setting to logrus.ErrorLevel, for example, would include errors, panics, and fatals, but not info or debug.
+			TimestampFormat: "Jan 2 15:04:05", // timeFormat could be an empty string instead; doing so will default to logrus's typically time format.
+			EncTLSConfig:    nil,              // setting config to nil means that conn will use root certs already set up on local system
+			UnencryptedTCP:  true,             // disable encryption, but still use TCP
+			UnencryptedUDP:  false,            // disable encryption and use UDP
+			UnencryptedPort: 514,              // omitting will result in port 514 usage; valid options are 80, 514, and 10000
+		},
 	)
 	if err != nil {
 		panic(err)
